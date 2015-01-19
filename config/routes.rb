d3a84bd877acf0da61adcbe257781b35286b55a6
Luis <luismenjivr@gmail.com>
2015-01-19 16:08:35 -0500

    installed devise, added devise views, added devise User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root "welcome#index"
+  devise_for :users
+  root to: "welcome#index"
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
