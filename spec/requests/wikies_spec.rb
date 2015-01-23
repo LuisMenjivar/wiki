28e50e551dc4c239cd1758c4c6d7403346dc1897
Luis <luismenjivr@gmail.com>
2015-01-23 17:32:08 -0500

    modified request spec and added authentication helper

--- spec/requests/wikies_spec.rb
+++ spec/requests/wikies_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
-
+include Devise::TestHelpers
 RSpec.describe "Wikies", :type => :request do
+ 
   describe "GET /wikies" do
-    it "works! (now write some real specs)" do
+    it "success when logged in" do
+      sign_in_as_a_valid_user
       get wikies_path
       expect(response).to have_http_status(200)
     end
+    it "redirects to sign up page when not signed in" do 
+      get wikies_path
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to(root_path)
+    end
+
   end
 end
