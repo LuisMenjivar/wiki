23ff3251adac0a825ca398ed24742f616e7b2955
Luis <luismenjivr@gmail.com>
2015-01-26 00:33:44 -0500

    added stripe monthly subscription and stripe feature spec coverage

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "welcome#index"
   get 'welcome/index'
+  resources :charges, only: [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
