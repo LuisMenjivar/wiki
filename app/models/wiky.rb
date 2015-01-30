ccfcf8403ce62e85065a2770b1351e09b87aac12
Luis <luismenjivr@gmail.com>
2015-01-29 21:42:42 -0500

    added wiky controller test coverage

--- app/models/wiky.rb
+++ app/models/wiky.rb
@@ -1,5 +1,6 @@
 class Wiky < ActiveRecord::Base
   belongs_to :user
+  validates :title, length: { minimum: 5 }
   scope :public_wikys, -> { where(public: true) }
   scope :private_wikys, -> {where(public: false)}
 end
