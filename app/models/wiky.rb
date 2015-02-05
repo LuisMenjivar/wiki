e3fcb51ecfb3a262b6f878a60b5ab55b7a7ff77d
Luis <luismenjivr@gmail.com>
2015-02-04 19:41:21 -0500

    added source to user and wiky models

--- app/models/wiky.rb
+++ app/models/wiky.rb
@@ -1,7 +1,7 @@
 class Wiky < ActiveRecord::Base
   belongs_to :user
   has_many :collaborations
-  has_many :collaborators, through: :collaborations, class_name: "User"
+  has_many :collaborators, through: :collaborations, source: "user"
   validates :title, length: { minimum: 5 }
   scope :public_wikys, -> { where(public: true) }
   scope :private_wikys, -> {where(public: false)}
