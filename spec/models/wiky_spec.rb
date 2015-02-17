0d5386f4d83655184e0c99fc140aae30e925a7b9
Luis <luismenjivr@gmail.com>
2015-02-17 14:51:39 -0500

    added search bar test coverage in wiky model spec

--- spec/models/wiky_spec.rb
+++ spec/models/wiky_spec.rb
@@ -16,4 +16,15 @@ RSpec.describe Wiky, :type => :model do
       expect((wiky.reload).public).to eq(true) 
     end 
   end
+
+  describe ".search_for" do 
+    it "searches for words in title or body of wiki" do 
+      tomato_wiki = Wiky.create!(title: "tomato", body: "red fruit" )
+      harry_wiki = Wiky.create!(title: "Harry Potter", body: "book series")
+      expect(Wiky.search_for("tomato")).to match_array(
+        Wiky.where("title LIKE '%#{"tomato"}%' OR body LIKE '%#{"tomato"}%'"))
+      expect(Wiky.search_for("book")).not_to match_array(
+        Wiky.where("title LIKE '%#{"tomato"}%' OR body LIKE '%#{"tomato"}%'"))
+    end
+  end
 end
