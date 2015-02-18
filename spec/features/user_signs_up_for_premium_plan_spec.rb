9b687b705370549f7df8b7fbd76677355c955ac2
Luis <luismenjivr@gmail.com>
2015-02-18 13:23:22 -0500

    modified features

--- spec/features/user_signs_up_for_premium_plan_spec.rb
+++ spec/features/user_signs_up_for_premium_plan_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 feature 'Premium Subscription' do
-  let(:user) { create(:user) }
-  scenario 'User signs up for premium wikys', js: true, slow: true do
-    sign_in(user.email, user.password)#sign_in method from spec/support/features/session_helpers.rb
-    expect(user.role).to eq(nil)
+  scenario 'User signs up for premium wikys', js: true, slow: true  do
+    user = User.new(id: 1, email: "test@example.com", password: "helloworld")
+    sign_up(user.email, user.password) #sign_up method in support/features/session_helpers
+    expect(user.reload.role).to eq("standard")
     visit new_charge_path
     click_button "Subscribe Now"
     sleep(5) # allows stripe_checkout_app frame to load
